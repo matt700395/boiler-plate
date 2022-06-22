35f2ccf6ef9540a82f8e8cfe2dc7ed3e6de95fb4
matt700395 <matt7003@korea.ac.kr>
2022-06-22 13:42:40 +0900

    20강 중간 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+
+        <Routes>
+          <Route exact path="/" element = {<Home />} />
+          <Route path="/about" element = {<About />} />
+          <Route path="/dashboard" element = {<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
